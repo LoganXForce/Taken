7b71202397852c30266af5ae4c5058e6d1aa8740
LoganXforce <joan990511@hotmail.com>
2021-02-15 19:14:07 -0600

    1.1

--- page-home.php
+++ page-home.php
@@ -7,7 +7,14 @@
     <title></title>
   </head>
   <body>
-<h1>hola mundo</h1>
+    <div id="verde">
+    </div>
+    <h1 id="Añade">Añade identidad a la cotidianidad</h1>
+    <div >
+      <img id="bannerimg1" src="https://paradise.tecnologyresorts.com/wp-content/uploads/2021/02/final_01-1.png" alt="">
+      <img id="bannerimg2"  src="https://paradise.tecnologyresorts.com/wp-content/uploads/2021/02/zoom_02-2.png" alt="">
+
+    </div>
   </body>
 </html>
 
