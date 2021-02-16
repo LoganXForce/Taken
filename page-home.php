09d0cf740a86c309eb9518efed370385688ed335
LoganXforce <joan990511@hotmail.com>
2021-02-15 20:15:46 -0600

    1.3

--- page-home.php
+++ page-home.php
@@ -10,10 +10,15 @@
     <div id="verde">
     </div>
     <h1 id="Añade">Añade identidad a la cotidianidad</h1>
-    <div >
+    <div id="banners1">
       <img id="bannerimg1" src="https://paradise.tecnologyresorts.com/wp-content/uploads/2021/02/final_01-1.png" alt="">
-      <img id="bannerimg2"  src="https://paradise.tecnologyresorts.com/wp-content/uploads/2021/02/zoom_02-2.png" alt="">
+      <img id="bannerimg2"  src="https://paradise.tecnologyresorts.com/wp-content/uploads/2021/02/Render_A_01_Detalle-1.png" alt="">
+        <img id="bannerimg3"  src="https://paradise.tecnologyresorts.com/wp-content/uploads/2021/02/zoom_02-2.png" alt="">
+    </div>
 
+    <div id="buttonbanner">
+      <button type="button" name="button1"> Prueba 1</button>
+      <button type="button" name="button2"> Prueba 2</button>
     </div>
   </body>
 </html>
